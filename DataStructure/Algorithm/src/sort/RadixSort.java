976ed3a42a931260bd83a3a495e42c7414db2b04
NCUTemp <jiaohelucky@gmail.com>
2022-05-28 16:19:35 -0400

    sort

---
+++ DataStructure/Algorithm/src/sort/RadixSort.java
@@ -0,0 +1,20 @@
+package sort;
+
+public class RadixSort {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        // 空间换时间的做法
+
+
+        int[] arr = {53,3,542,748,14,214};
+
+    }
+
+    //  10 个桶
+    public static void radixSort() {
+//        int[][] bocket = new int[10][arr.length];
+
+    }
+
+}
